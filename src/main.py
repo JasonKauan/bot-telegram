5157ec9557dd52819da8b79eccead2b6b796f6b6
Raphael Victor Freitas Ramos de Alcântara <raphaelvictor40@gmail.com>
2025-09-28 11:43:00 -0300

    Hello bot

--- src/main.py
+++ src/main.py
@@ -2,10 +2,16 @@ from env import bot_config
 import telebot
 
 api = bot_config.API_TOKEN
-print(api)
 bot = telebot.TeleBot(api)
 
 
+@bot.message_handler(commands=['start', 'help'])
+def responder(message):
+    bot.reply_to(message, "Hello, I am a bot!")
 
 
+
+
+
+bot.polling(none_stop=True)
 
